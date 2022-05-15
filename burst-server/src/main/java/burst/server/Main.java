2e4abedaca4de997194228335e96b8506c69c615
fzdwx <likelovec@gmail.com>
2022-05-15 21:47:25 +0800

    init

---
+++ burst-server/src/main/java/burst/server/Main.java
@@ -0,0 +1,12 @@
+package burst.server;
+
+/**
+ * @author <a href="mailto:likelovec@gmail.com">fzdwx</a>
+ * @date 2022/5/15 21:43
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
