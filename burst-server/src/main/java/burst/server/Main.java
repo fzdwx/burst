a4d9e83408861fbff7ffed22b18753b746573475
fzdwx <likelovec@gmail.com>
2022-05-16 23:03:11 +0800

    test sky spring boot starter

--- burst-server/src/main/java/burst/server/Main.java
+++ burst-server/src/main/java/burst/server/Main.java
@@ -1,13 +1,20 @@
 package burst.server;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
 /**
  * @author <a href="mailto:likelovec@gmail.com">fzdwx</a>
  * @date 2022/5/15 21:43
  */
+@RestControllerAdvice
+@SpringBootApplication
 public class Main {
 
     public static void main(String[] args) {
-
-        System.out.println("Hello world!");
+        final ConfigurableApplicationContext run = SpringApplication.run(Main.class);
     }
+
 }
