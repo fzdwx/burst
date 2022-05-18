83e024126d55aa1fea2d82b9c349fdd0c6eda2ee
weiZhen <likelovec@gmail.com>
2022-05-18 19:17:36 +0800

    test

---
+++ burst-server/src/main/java/burst/server/controller/TestController.java
@@ -0,0 +1,20 @@
+package burst.server.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author <a href="mailto:likelovec@gmail.com">fzdwx</a>
+ * @date 2022/5/17 11:51
+ */
+@RestController
+@RequestMapping("/test")
+public class TestController {
+
+    @GetMapping("/hello/{name}")
+    public String hello() {
+        return "hello world";
+    }
+}
