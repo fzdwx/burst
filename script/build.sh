7e881fc4e7ba3e840b0965926644f380a79ee82e
weiZhen <likelovec@gmail.com>
2022-06-15 11:25:12 +0800

    test

--- script/build.sh
+++ script/build.sh
@@ -2,14 +2,18 @@ VERSION=1.3
 ACTION=$1
 
 function build_server() {
+    echo "start build server docker image"
     cd `dirname $0`
     CURRENT_DIR=`pwd`
+    echo $CURRENT_DIR
     cd ../burst-server/target
     CONTEXT_DIR=`pwd`
+    echo $CONTEXT_DIR
     echo "docker build -t fzdwx/burst-server:$VERSION -f $CURRENT_DIR/Dockerfile $CONTEXT_DIR"
+    docker build -t fzdwx/burst-server:$VERSION -f $CURRENT_DIR/Dockerfile $CONTEXT_DIR
 }
 
-case "$ACTION$" in
+case "$ACTION" in
 server)
     build_server
   ;;
