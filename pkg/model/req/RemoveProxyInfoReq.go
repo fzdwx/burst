87c61041e40d447fcc21b10ab3de31a90282c945
fzdwx <likelovec@gmail.com>
2022-07-06 22:22:51 +0800

    some code

---
+++ pkg/model/req/RemoveProxyInfoReq.go
@@ -0,0 +1,66 @@
+package req
+
+import (
+	"fmt"
+	"github.com/fzdwx/burst"
+	"github.com/fzdwx/burst/pkg"
+	"github.com/fzdwx/burst/pkg/model"
+	"net"
+)
+
+type (
+	RemoveProxyInfoReq struct {
+		Proxy []RemoveProxyInfo `json:"proxy"`
+	}
+
+	RemoveProxyInfo struct {
+		Ip          string `json:"ip"`
+		Port        int    `json:"port"`
+		ChannelType string `json:"channelType,default=tcp"`
+	}
+)
+
+func (i RemoveProxyInfo) Addr() string {
+	return burst.FormatAddr(i.Ip, i.Port)
+}
+
+func (i RemoveProxyInfo) String() string {
+	return fmt.Sprintf("%s:%s", i.ChannelType, i.Addr())
+}
+
+func (i RemoveProxyInfo) ToCache() *pkg.ServerProxyInfo {
+	return &pkg.ServerProxyInfo{
+		Ip:          i.Ip,
+		Port:        i.Port,
+		ChannelType: i.ChannelType,
+		Addr:        i.Addr(),
+	}
+}
+
+func (r RemoveProxyInfoReq) Check() error {
+	if len(r.Proxy) == 0 {
+		return model.ProxyIsRequired
+	}
+
+	for _, info := range r.Proxy {
+		if info.Ip == burst.EmptyStr {
+			return model.IpIsNotValid
+		}
+
+		if info.Ip != "localhost" {
+			ip := net.ParseIP(info.Ip)
+			if ip == nil {
+				return model.IpIsBlank
+			}
+		}
+
+		if info.Port <= 0 || info.Port > 65535 {
+			return model.PortIsNotValid
+		}
+
+		if !pkg.CheckChannelType(info.ChannelType) {
+			return pkg.ErrChannelTypeNotValid
+		}
+	}
+	return nil
+}
--- server/api/proxy/removeProxy.go
+++ server/api/proxy/removeProxy.go
@@ -1,12 +1,20 @@
 package proxy
 
 import (
+	"github.com/fzdwx/burst"
+	"github.com/fzdwx/burst/pkg/model"
+	"github.com/fzdwx/burst/pkg/result"
 	"github.com/fzdwx/burst/server/svc"
 	"net/http"
 )
 
 func RemoveProxy(svcContext *svc.ServiceContext) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
+		token := burst.GetQuery("token", r)
+		if token == burst.EmptyStr {
+			result.HttpBadRequest(w, model.TokenIsRequired.Error())
+			return
+		}
 
 		// todo remove proxy
 		// 	1. close listener
